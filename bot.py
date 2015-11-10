26207c36e15a1993e4fa45acb64bf6925d8736ff
Foxscotch <foxscotch@gmail.com>
2015-11-10 08:06:36 -0500

    THESE CHANGES. i NEEEED to write the changelogs for them. REMEMBER TO DO THIS

--- bot.py
+++ bot.py
@@ -1,13 +1,11 @@
 import requests
-import pickle
 import json
 
-
-class TelegramError(Exception):
-    pass
+from .main import TelegramError, User
 
 
-class Bot:
+class Bot(User):
+    # noinspection PyMissingConstructor
     def __init__(self, api_key, initial_check=False, **kwargs):
         """
         Init function for Bot.
@@ -56,14 +54,6 @@ class Bot:
 
         return 'Bot(api_key="{0}", initial_check=True)'.format(self.api_key)
 
-    def __str__(self):
-        """
-        The str function for Bot. It returns the first_name of the bot, and it's
-        mostly just intended to be the name of the file for serialization.
-        """
-
-        return self.first_name
-
     def update_info(self):
         """
         Updates the bot's info.
@@ -90,56 +80,6 @@ class Bot:
                 raise TelegramError('The result was not "ok"')
         else:
             raise TelegramError('Did not get a 200 response', r.status_code)
-
-    def serialize_to_bytes(self):
-        """
-        Creates and returns a pickle string representation of the instance.
-
-        :return: Pickle serialization string
-        :rtype: bytes
-        """
-
-        return pickle.dumps(self)
-
-    def serialize_to_file(self, filename=None):
-        """
-        Creates and saves a pickle file for the instance. By default, it uses
-        "first_name.pkl" as the file name, where first_name is the object's real
-        first_name attribute. Then it returns the file name.
-
-        :param str filename: File name you want to save as
-        :return: File name it was saved as
-        :rtype: str
-        """
-
-        if not filename:
-            filename = self.__str__() + '.pkl'
-        pickle.dump(self, open(filename, 'wb+'))
-        return filename
-
-    @staticmethod
-    def deserialize_from_bytes(deserialization_string):
-        """
-        Loads a pickle representation of an object from a bytes object.
-
-        :param bytes deserialization_string: Bytes object to load from
-        :return: The Bot instance that was loaded
-        :rtype: Bot
-        """
-
-        return pickle.loads(deserialization_string)
-
-    @staticmethod
-    def deserialize_from_file(filename):
-        """
-        Loads a pickle representation of an object from a file.
-
-        :param str filename: Name of file to load from
-        :return: The Bot instance that was loaded
-        :rtype: Bot
-        """
-
-        return pickle.load(open(filename, 'rb'))
 
 
 example_bot = Bot(api_key='123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11',
---
+++ main.py
@@ -0,0 +1,96 @@
+import requests
+import pickle
+import json
+
+
+class TelegramError(Exception):
+    pass
+
+
+class User:
+    def __init__(self, user_id, first_name, last_name=None, username=None):
+        """
+        Init function for User.
+
+        :param int user_id: User's ID
+        :param str first_name: User's first name
+        :param str last_name: User's last name, optional
+        :param str username: User's username, optional
+        """
+
+        self.user_id = user_id
+        self.first_name = first_name
+
+        if last_name:
+            self.last_name = last_name
+        if username:
+            self.username = username
+
+    def __repr__(self):
+        """
+        The repr function for User. It's not a perfect representation of the
+        very same object, but it's descriptive enough.
+        """
+
+        return 'User(user_id={0}, first_name{1})'.format(self.user_id,
+                                                         self.first_name)
+
+    def __str__(self):
+        """
+        The str function for User. It returns the first_name of the user, and
+        it's mostly just intended to be the name of the file for serialization.
+        Because first names don't have to be unique, it probably isn't a very
+        good idea to use this in important contexts.
+        """
+
+        return self.first_name
+
+    def serialize_to_bytes(self):
+        """
+        Creates and returns a pickle string representation of the instance.
+
+        :return: Pickle serialization string
+        :rtype: bytes
+        """
+
+        return pickle.dumps(self)
+
+    def serialize_to_file(self, filename=None):
+        """
+        Creates and saves a pickle file for the instance. By default, it uses
+        "first_name.pkl" as the file name, where first_name is the object's real
+        first_name attribute. Then it returns the file name.
+
+        :param str filename: File name you want to save as
+        :return: File name it was saved as
+        :rtype: str
+        """
+
+        if not filename:
+            filename = self.__str__() + '.pkl'
+        pickle.dump(self, open(filename, 'wb+'))
+        return filename
+
+    @staticmethod
+    def deserialize_from_bytes(deserialization_string):
+        """
+        Loads a pickle representation of an object from a bytes object.
+
+        :param bytes deserialization_string: Bytes object to load from
+        :return: The instance that was loaded
+        :rtype: User
+        """
+
+        return pickle.loads(deserialization_string)
+
+    @staticmethod
+    def deserialize_from_file(filename):
+        """
+        Loads a pickle representation of an object from a file.
+
+        :param str filename: Name of file to load from
+        :return: The instance that was loaded
+        :rtype: User
+        """
+
+        return pickle.load(open(filename, 'rb'))
